5aa3efba182c4150d5c3d45de92260614ef85034
Vikash Khati <vikashkhati007@gmail.com>
2024-10-01 20:12:29 +0530

    auth added..

---
+++ lib/auth.ts
@@ -0,0 +1,15 @@
+import { AuthOptions, getServerSession } from "next-auth";
+import GoogleProvider from "next-auth/providers/google";
+
+const authOptions: AuthOptions = {
+  providers: [
+    GoogleProvider({
+      clientId: process.env.GOOGLE_CLIENT_ID!,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET!,
+    }),
+  ],
+};
+
+const getSession = () => getServerSession(authOptions);
+
+export { authOptions, getSession };
